16b50fb826ec78035c9a45b5b85719f72387b3b5
Fighoture <yao19980220@gmail.com>
2024-11-06 17:15:25 +0800

    fix target path bug

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+#prune_result="/vepfs/hongcheng/moe/MoE_unsupervised_pruning/pruned_result/sample_500_cluster_12/in_class_prune_0.2"
+prune_result="/vepfs/hongcheng/moe/MoE_unsupervised_pruning/pruned_result/sample_5000/seer_prune_0.2"
+#prune_result="."
 CUDA_VISIBLE_DEVICES="0,1" evalplus.evaluate --model "/vepfs/hongcheng/moe/modelscope_cache/deepseek-ai/DeepSeek-V2-Lite" \
-                  --prune_result "." \
+                  --prune_result $prune_result \
                   --dataset "humaneval" \
                   --backend "hf" \
                   --greedy
