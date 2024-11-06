560f74b277d20387fd45a7a6a94ef84fbca5cd02
Fighoture <yao19980220@gmail.com>
2024-11-06 15:13:14 +0800

    update run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-evalplus.evaluate --model "" \
-                  --prune_result "" \
+CUDA_VISIBLE_DEVICES="0,1" evalplus.evaluate --model "/vepfs/hongcheng/moe/modelscope_cache/deepseek-ai/DeepSeek-V2-Lite" \
+                  --prune_result "." \
                   --dataset "humaneval" \
                   --backend "hf" \
-                  --bs 8
+                  --greedy
